c7ce622addc7a17c406beffb4797e05ae75fb7c8
ThanapornA <thanaporn.apir@bumail.net>
2025-03-26 12:27:21 +0700

    first publish
    map setup + obstacles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputAction moveAction;
+    private Rigidbody rb;
+    public int speed;
+    public int turnSpeed = 100;
+
+    public int Health = 5;
+    public int HoneyCollected = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        float horiAction = moveAction.ReadValue<Vector2>().x;
+        float verticalAction = moveAction.ReadValue<Vector2>().y;
+        rb.AddForce( verticalAction * transform.right * speed , ForceMode.Force);
+        
+        if (horiAction!= 0)
+        {
+            transform.Rotate( Vector3.up , horiAction * turnSpeed * Time.deltaTime);
+        }
+/*
+        if ( Health == 0 )
+        {
+
+        }*/
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("obstacles"))
+        {
+            Health -= 1;
+            Debug.Log("you hit" + Health);
+        }
+
+        /*if (collision.gameObject.CompareTag("honey"))
+        {
+            HoneyCollected += 1;
+        }*/
+    }
+/*
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("trigger"))
+        {
+            Debug.Log("you triggered");
+        }
+    }*/
+}
